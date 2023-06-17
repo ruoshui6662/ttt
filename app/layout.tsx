f4b1a11efd1599e1b38807b4e2fa9892af2ecce6
ruoshui6662 <110121567+ruoshui6662@users.noreply.github.com>
2023-06-17 10:05:20 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "若水GPT",
+  description: "AI助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
